bdb563d835cf4537299dc8e7b1d90d47308da0e6
Yar <slvwinswim@gmail.com>
2023-08-17 21:23:32 +0300

    added calculation form

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
 import showMoreStiles from './modules/showMoreStiles';
+import calc from './modules/calc';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -16,4 +17,5 @@ window.addEventListener('DOMContentLoaded', () => {
     checkTextInputs('[name="name"]');
     checkTextInputs('[name="message"]');
     showMoreStiles('.button-styles', '#styles .row');
+    calc('#size', '#material', '#options', '.promocode', '.calc-price');
 });
